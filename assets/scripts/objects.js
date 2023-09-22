b340bbc47de435cb6970363a08aba9bba593b961
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-09-22 20:21:25 +0300

    accessed et set properties dynamically

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -1,13 +1,16 @@
+const userChosenKeyName = 'level';
+
 const person = {
   'first name': 'Max',
   age: 30,
   hobbies: ['Sports', 'Cooking'],
+  [userChosenKeyName]: '...', // sets dynamically the property
   greet: function () {
     alert('Hi there!');
   },
   1.5: 'hello'
 };
 
-console.log(person[1.5]);
-// console.log(person['1.5']); // it also works because JavaScript automatically coerces values to strings
-console.log(person); // Look at the order of the object in the browser console (expanded object and not expanded)
+const keyName = 'first name';
+
+console.log(person[keyName]); // accesses dynamically the property
