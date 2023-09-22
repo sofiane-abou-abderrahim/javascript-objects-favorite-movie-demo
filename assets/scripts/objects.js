892179ebbd261ae92feea9d05e16e923d78ec1a0
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-09-22 18:35:20 +0300

    added, modified and deleted properties of an object

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -7,4 +7,20 @@ const person = {
   }
 };
 
-person.greet();
+///// ADDING /////
+
+person.isAdmin = true; // adds a new property
+
+///// MODIFYING /////
+
+person.age = 31; // overrides the old value
+
+console.log(person);
+
+///// DELETING /////
+
+delete person.age; // cleaner than setting it to undefined because you really get rid of it
+// person.age = undefined; // you don't see the property in the browser console, but technically it is still there
+// person.age = null; // still has the age property, but it resets the value
+
+console.log(person.age, person);
