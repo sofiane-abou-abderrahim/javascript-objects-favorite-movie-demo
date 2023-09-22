4b8bbc2a710801f2f332bc4443696ddce41da2c8
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-09-22 22:57:11 +0300

    created the logic to add a new movie by clicking the add movie button and show the object in the browser console

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -1,18 +1,30 @@
-const userChosenKeyName = 'level';
+const addMovieBtn = document.getElementById('add-movie-btn');
+const searchBtn = document.getElementById('search-btn');
 
-const person = {
-  'first name': 'Max',
-  age: 30,
-  hobbies: ['Sports', 'Cooking'],
-  [userChosenKeyName]: '...', // sets dynamically the property
-  greet: function () {
-    alert('Hi there!');
-  },
-  1.5: 'hello'
+const movies = []; // an array is an object (reference type), we can store this in a constant and still edit the value here with push and pop and so on
+
+const addMovieHandler = () => {
+  const title = document.getElementById('title').value;
+  const extraName = document.getElementById('extra-name').value;
+  const extraValue = document.getElementById('extra-value').value;
+
+  if (
+    title.trim() === '' ||
+    extraName.trim() === '' ||
+    extraValue.trim() === ''
+  ) {
+    return;
+  }
+
+  const newMovie = {
+    info: {
+      title, // useful shorthand notation, if key name and value name are the same, you can use that instead of "title: title"
+      [extraName]: extraValue
+    },
+    id: Math.random()
+  };
+  movies.push(newMovie);
+  console.log(newMovie);
 };
 
-const keyName = 'first name';
-
-console.log(person[keyName]); // accesses dynamically the property
-console.log(person[userChosenKeyName]); // // accesses dynamically the property
-// console.log(person['level']); // you can access dynamically the property like this as well
+addMovieBtn.addEventListener('click', addMovieHandler);
