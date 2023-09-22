a4290f604af32b64857dd2a95a7899317382cd99
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-09-22 19:41:47 +0300

    added number as a property of the object & checked the order of the object in the browser console

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -4,12 +4,10 @@ const person = {
   hobbies: ['Sports', 'Cooking'],
   greet: function () {
     alert('Hi there!');
-  }
+  },
+  1.5: 'hello'
 };
 
-console.log(person['first name']);
-
-const movieList = document.getElementById('movie-list');
-// movieList.style.backgroundColor = 'red';
-movieList.style['background-color'] = 'red'; // square brackets access feature
-movieList.style.display = 'block';
+console.log(person[1.5]);
+// console.log(person['1.5']); // it also works because JavaScript automatically coerces values to strings
+console.log(person); // Look at the order of the object in the browser console (expanded object and not expanded)
