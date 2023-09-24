cf2030426ab19a548d4b970485d9461adf26eb98
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-09-24 18:21:49 +0300

    commented what does the spread operator

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -1,5 +1,13 @@
 const person = { name: 'Max', hobbies: ['Sports', 'Cooking'] };
 
+// const person2 = { ...person };
+/*
+What this does is it takes all key-value pairs of that object you pass after the spread operator
+and merge these key-value pairs into this object.
+So it creates a new object but copies over the values you have in the keys of the old object
+and not only values but also the key names.
+*/
+
 const person2 = { ...person, age: 30, hobbies: [...person.hobbies] };
 
 console.log(person);
