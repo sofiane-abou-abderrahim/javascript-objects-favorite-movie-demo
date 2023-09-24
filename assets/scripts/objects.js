ea94cfb2944914f9154c2bbfec292a6b6503a889
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-09-24 12:34:23 +0300

    object with only number keys it is sorted automatically in the browser console

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -11,3 +11,10 @@ const person = {
 console.log(person[1.5]);
 // console.log(person['1.5']); // it also works because JavaScript automatically coerces values to strings
 console.log(person); // Look at the order of the object in the browser console (expanded object and not expanded)
+
+// There is one important exception,
+// if we have an object which only consists of only number keys,
+// then if you output "numbers", you see indeed here that is sorted
+// => That makes sense because arrays are objects with number keys where the order should be guaranteed (ascending)!
+const numbers = { 5: 'hi', 1: 'true' };
+console.log(numbers);
